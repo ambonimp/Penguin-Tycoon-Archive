09facb5e66c2ba7c4cd20f10cb556afab71c8ca4
Leonardo Vasques <quenixserra@gmail.com>
2022-06-07 15:56:58 -0300

    Ignore analytics events in DEV place

--- src/city/ServerScriptService/PlayfabAnalytics.server.lua
+++ src/city/ServerScriptService/PlayfabAnalytics.server.lua
@@ -2,8 +2,16 @@ local ServerStorage = game:GetService("ServerStorage")
 local EventHandler = ServerStorage:FindFirstChild("EventHandler")
 local AnalyticsService = game:GetService("AnalyticsService")
 
+-- Constants
+local CITY_PLACE_ID = 7967681044
+
 EventHandler.Event:Connect(function(event, player, data)
-    -- Tries to post Playfab event
+    -- Ignore in case this is not the main place
+	if game.PlaceId ~= CITY_PLACE_ID then
+		return
+	end
+
+	-- Tries to post Playfab event
 	pcall(function()
 		AnalyticsService:FireCustomEvent(player, event, data)
 	end)
